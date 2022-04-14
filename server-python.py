7dfede06b322e2a6dea8a6bda6191c3a7feb2659
minichibis <minichibisart@gmail.com>
2022-04-14 13:45:04 -0500

    pushed client and server to the actual right branvch instead of "master"

---
+++ server-python.py
@@ -0,0 +1,52 @@
+###############################################################################
+# server-python.py
+# Name: Sam Carpenter
+###############################################################################
+
+import sys
+import socket
+
+RECV_BUFFER_SIZE = 2048
+QUEUE_LENGTH = 10
+queuesize = 0
+
+def server(server_port):
+    """TODO: Listen on socket and print received message to sys.stdout"""
+    thesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    print("connecting the server to", host)
+    thesocket.bind((host, server_port))
+    thesocket.listen(QUEUE_LENGTH)
+    while True:
+        newsocket, address = thesocket.accept()
+        handlenewconnection(newsocket)
+    pass
+
+def handlenewconnection(newsocket):
+    gotted = []
+    while True:
+        getted = newsocket.recv(RECV_BUFFER_SIZE)
+        #if getted == b'':
+        #    raise RuntimeError("socket connection broken")
+        #el
+        if len(getted) == 0:
+            if len(gotted) == 0:
+                raise RuntimeError("socket connection broken")
+            else:
+                s = ""
+                for c in gotted:
+                    s += c
+                print(s)
+                break
+        else:
+            gotted.append(getted.decode('utf-8'))
+
+def main():
+    """Parse command-line argument and call server function """
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python server-python.py [Server Port]")
+    server_port = int(sys.argv[1])
+    server(server_port)
+
+if __name__ == "__main__":
+    main()
